d8e186d0f13f63965ee6fb13cd006d0b65f5dc34
NicoTuxx <nicolasstempert@gmail.com>
2017-04-23 03:43:20 +0200

    feat: implement basic light features
    - generic 'sendProcess' function to communicate with the API
    - scan of existing lights at page init
    - loading of user process list
    - basic 'toggleLight' function to turn off and on a light

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import { processes } from '../assets/processes';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  selectedProcess: string;
+  selectedLight: string;
+  lightOn: boolean;
+  processList: Object;
+
+  constructor(private http: Http) {
+    this.lightOn = false;
+    this.processList = processes;
+  }
+
+  ngOnInit(): void {
+    this.http.get(`${environment.apiUrl}/process/SCAN`).subscribe();
+  }
+
+  toggleLight(light: string): void {
+    this.lightOn = ! this.lightOn;
+
+    const process = this.lightOn ? 'POWER_ON' : 'POWER_OFF';
+    this.sendProcess(process, light);
+  }
+
+  sendProcess(process: string, light?: string): void {
+    let url = `${environment.apiUrl}/process/${process}`;
+
+    if (light) {
+      url += `/light/${light}`;
+    }
+
+    this.http.get(url).subscribe();
+  }
 }
